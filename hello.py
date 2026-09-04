f812eb08fe151f52111f5cec16f3db9fb5c34413
CY-SIT <2601842@sit.singaporetech.edu.sg>
2026-09-04 10:46:12 +0800

    activity 1

--- hello.py
+++ hello.py
@@ -3,15 +3,4 @@ print ("welcome here")
 print("my first post!")
 print("===========================")
 
-username = input("Enter your username: ")
-age = int(input("Enter your age: "))
-category = input("Enter your category: ")
 
-print ("\nInstagram Profile")
-print("===========================")
-print("Username:", username)
-print("age:", age)
-print("category: ", category)
-
-if age>40 and category =="fun":
-    print("You are old what is fun for you?")
