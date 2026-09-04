cebbf3b8ca596cedc2cd844b85782ed2a6803ce3
CY-SIT <2601842@sit.singaporetech.edu.sg>
2026-09-04 10:53:57 +0800

    activity 3

--- hello.py
+++ hello.py
@@ -10,4 +10,10 @@ followers = 100
 
 print("Username:", username)
 print("Bio:", bio)
-print("Followers:", followers)
+
+followers += 50
+print("Day 1: Followers:", followers)
+followers += 20
+print("Day 2: Followers:", followers)
+followers += 10
+print("Day 3: Followers:", followers)
