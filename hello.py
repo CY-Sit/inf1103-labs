41961dc0f454e121d0c72a834b50f8917f390a2d
CY-SIT <2601842@sit.singaporetech.edu.sg>
2026-09-04 10:18:49 +0800

    activity 2

--- hello.py
+++ hello.py
@@ -1,1 +1,21 @@
-print ("hello world")
+print("===========================")
+print ("welcome here")
+print("my first post!")
+print("===========================")
+
+username = "cool_creator"
+bio = "Fun blogger"
+followers = 100
+
+print ("Username:", username)
+print ("Bio:", bio)
+print ("Followers:", followers)
+
+followers +=50
+print("Day 1:" , followers)
+
+followers +=20
+print("Day 2:" , followers)
+
+followers += 10
+print("Day 3:" , followers)
